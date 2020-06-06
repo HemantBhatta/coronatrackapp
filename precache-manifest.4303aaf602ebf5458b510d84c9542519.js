0a262ae94548e9afcc5fb5c775eba86c5d11d432
HemantBhatta <hemub70@gmail.com>
2020-06-06 11:09:51 +0545

    Updates

---
+++ precache-manifest.4303aaf602ebf5458b510d84c9542519.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cf849a0d02d2d9e1f8ce2f4d6ce1491",
+    "url": "/coronatrackapp/index.html"
+  },
+  {
+    "revision": "6fb09276fd1a026f2140",
+    "url": "/coronatrackapp/static/css/main.7107a1f5.chunk.css"
+  },
+  {
+    "revision": "920e3107747acb9e9044",
+    "url": "/coronatrackapp/static/js/2.fcb21244.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/coronatrackapp/static/js/2.fcb21244.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fb09276fd1a026f2140",
+    "url": "/coronatrackapp/static/js/main.0375d8ed.chunk.js"
+  },
+  {
+    "revision": "6bfc837b0b1db84cd84b",
+    "url": "/coronatrackapp/static/js/runtime-main.5c85436f.js"
+  },
+  {
+    "revision": "e9ea73949de71d45ae46047646b95727",
+    "url": "/coronatrackapp/static/media/loading-arrow.e9ea7394.gif"
+  }
+]);
